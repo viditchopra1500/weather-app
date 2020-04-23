58bff3f4e4eba721fe57c9d9c3685d8294dbb5dd
vidit chopra <51998522+viditchopra1500@users.noreply.github.com>
2020-04-24 02:37:53 +0530

    final app.js file

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const request=require('request')
-const yargs=require('yargs')
+const yargs=require('yargs');
+const axios=require('axios');
 
 const argv=yargs
 .options({
     a:{
         demand:true,
-        alias:'adress',
+        alias:'address',
         describe:'Address to fetch weather for',
         string:true
     }
@@ -13,25 +13,30 @@ const argv=yargs
 .alias('help','h')
 .argv;
 
-var address=argv.a;
-encodeURIComponent(address);
-console.log(address);
-request({
-    url:'https://geocoder.ls.hereapi.com/6.2/geocode.json?searchtext='+address+'&gen=9&apiKey=Bm_SSsLxeHfOQd0r8-WDSD-0TY46-mgCLZjlm2ld-jI',
-    json:true
-},(error,response,body)=>{
-    if(error){
-        console.log('Unable to connect to server.');
+address=encodeURIComponent(argv.address);
+var geocodeUrl='https://geocoder.ls.hereapi.com/6.2/geocode.json?searchtext='+address+'&gen=9&apiKey=Bm_SSsLxeHfOQd0r8-WDSD-0TY46-mgCLZjlm2ld-jI'
+axios.get(geocodeUrl).then((response)=>{
+    if(response.data.Response.View.length===0){
+        throw new Error('Unable to find that address')
     }
-    else if(body.Response.View.length!==0){
-    var Address=body.Response.View[0].Result[0].Location.Address.Label;
-    var latitude=body.Response.View[0].Result[0].Location.NavigationPosition[0].Latitude;
-    var longitude=body.Response.View[0].Result[0].Location.NavigationPosition[0].Longitude;
-    console.log(`Address: ${Address}.`);
-    console.log(`Latitude: ${latitude}.`);
-    console.log(`Longitude: ${longitude}.`);
+    var address= response.data.Response.View[0].Result[0].Location.Address.Label;
+
+    var latitude=response.data.Response.View[0].Result[0].Location.NavigationPosition[0].Latitude;
+
+    var longitude=response.data.Response.View[0].Result[0].Location.NavigationPosition[0].Longitude;
+
+    var weatherUrl=`https://weather.ls.hereapi.com/weather/1.0/report.json?product=observation&latitude=${latitude}&longitude=${longitude}&oneobservation=true&apiKey=Bm_SSsLxeHfOQd0r8-WDSD-0TY46-mgCLZjlm2ld-jI`
+
+    console.log(address);
+    return axios.get(weatherUrl);
+}).then((response)=>{
+            temperature=response.data.observations.location[0].observation[0].temperature,
+            apparentTemperature=response.data.observations.location[0].observation[0].highTemperature
+            console.log(`Its currently ${temperature}.It feels like ${apparentTemperature}`)
+}).catch((e)=>{
+    if(e.code==='ENOTFOUND'){
+        console.log('Unable to connect to API servers')
+    }else{
+        console.log(e.message);
     }
-    else 
-    console.log('Address not found.');
-    
-}) 
+})
